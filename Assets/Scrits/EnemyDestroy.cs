9ba00ce163a3086d7c833f7e8052cfec61828517
woods-one <0820ricer@gmail.com>
2022-02-23 23:17:22 +0900

    add : スクリプトにコメントアウトの追加

--- Assets/Scrits/EnemyDestroy.cs
+++ Assets/Scrits/EnemyDestroy.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 敵オブジェクトの消去処理
+/// </summary>
 public class EnemyDestroy : MonoBehaviour
 {
+    /// <summary>
+    /// プレイヤーの弾が着弾時消去
+    /// </summary>
     public void DestroyEnemyObject()
     {
         Destroy(this.gameObject);
--- Assets/Scrits/PlayerDestroy.cs
+++ Assets/Scrits/PlayerDestroy.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// プレイヤーオブジェクトの消去処理
+/// </summary>
 public class PlayerDestroy : MonoBehaviour
 {
+    /// <summary>
+    /// 敵の弾が着弾時消去
+    /// </summary>
     public void DestroyPlayerObject()
     {
         Destroy(this.gameObject);
