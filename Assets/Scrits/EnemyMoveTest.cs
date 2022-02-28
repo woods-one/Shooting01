3282803fd9e08b190f5300c25e8e2911f93b563e
woods-one <0820ricer@gmail.com>
2022-03-01 01:55:15 +0900

    test : 敵の動きに法則性をつけた

--- Assets/Scrits/EnemyMoveTest.cs
+++ Assets/Scrits/EnemyMoveTest.cs
@@ -12,14 +12,57 @@ public class EnemyMoveTest : MonoBehaviour
     [SerializeField]
     float hoge2;
 
+    [SerializeField]
+    float horizontalSpeed;
+    [SerializeField]
+    float downSpeed;
+
+    bool isRight;
+
     void Start()
     {
+        isRight = true;
         firstpos = this.transform.position;
+        sin = 0;
+        cos = 0;
     }
-    void Update()
+    void FixedUpdate()
     {
         sin = Mathf.Sin(Time.time);
         cos = Mathf.Cos(Time.time);
+        firstpos.y -= downSpeed;
+        if(isRight)
+        {
+            MoveRight();
+        }
+        else
+        {
+            MoveLeft();
+        }
+        
+    }
+
+    void MoveRight()
+    {
+        firstpos.x += horizontalSpeed;
         this.transform.position = new Vector3(firstpos.x + sin * hoge, firstpos.y + cos * hoge2, 0);
+    }
+
+    void MoveLeft()
+    {
+        firstpos.x -= horizontalSpeed;
+        this.transform.position = new Vector3(firstpos.x - sin * hoge, firstpos.y + cos * hoge2, 0);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "RightWall")
+        {
+            isRight = false;
+        }
+        if (other.gameObject.name == "LeftWall")
+        {
+            isRight = true;
+        }
     }
 }
